a1e42f2a1f1b4122f8d53fdd2168862d4396321f
Asudy <18906052155@189.cn>
2020-08-26 19:25:25 +0800

    Added functions for course info & assignment operations

--- Code/2-4/src/teacher.sh
+++ Code/2-4/src/teacher.sh
@@ -17,13 +17,13 @@ show_teacher_menu() {
     echo "    7)  修改课程信息"
     echo "    8)  删除课程信息"
     echo "    9)  显示课程信息列表"
-    # echo "    10) 查询课程信息"
+    echo "    10) 查询课程信息"
     echo "布置作业相关操作："
     echo "    11) 创建作业"
     echo "    12) 修改作业"
     echo "    13) 删除作业"
     echo "    14) 显示作业列表"
-    # echo "    15) 查询作业"
+    echo "    15) 查询作业"
     echo "账号管理："
     echo "    16) 修改密码"
     echo
@@ -55,12 +55,12 @@ modify_student_account() {
         return 1
     fi
     
-    oldName=`echo $sinfo | cut -d: -f2`
+    oldContent=`echo $sinfo | cut -d: -f2`
     oldPass=`echo $sinfo | cut -d: -f3`
-    read -p "更改学生姓名（留空则保持不变）：$oldName -> " sname
+    read -p "更改学生姓名（留空则保持不变）：$oldContent -> " sname
     read -p "更改学生密码（留空则保持不变）：$oldPass -> " spass
     if [ "$sname" -o "$spass" ] ; then  # 若有任一更改，更新文件
-        sed -i "/^s$sid:/c\s$sid:${sname:=$oldName}:${spass:=$oldPass}" $userInfoFile
+        sed -i "/^s$sid:/c\s$sid:${sname:=$oldContent}:${spass:=$oldPass}" $userInfoFile
         echo "学号 $sid 学生账户信息修改成功！当前姓名：$sname；当前密码：$spass"
     else
         echo "学号 $sid 学生账户信息未进行任何修改"
@@ -108,37 +108,175 @@ search_student() {
     return 0
 }
 
-# create_course_info() {
+# 创建课程信息，成功返回0、失败返回1。
+create_course_info() {
+    [ ! -e $courseInfoFile ] && echo "课程信息文件不存在，请联系管理员创建课程！" && return 1    # 检查文件是否存在
+    read -p "请输入课程信息编号：m" mid     # 为方便区分，课程信息编号前缀'm'
+    grep "^m$mid:" $courseInfoFile 1>/dev/null 2>&1
+    if [ $? -eq 0 ] ; then              # 编号为主键，重复则报错
+        echo "该编号课程信息已存在，创建失败！"
+        return 1
+    fi
+    read -p "请设置该课程信息所属课程：c" cid
+    grep -qs "^c$cid:" $courseInfoFile
+    if [ $? -ne 0 ] ; then
+        echo "该课程不存在，课程信息创建失败！"
+        return 1
+    else
+        grep -qs "^c$cid:.*:$1$" $courseInfoFile
+        [ $? -ne 0 ] && echo "您不是该课程的任课老师，课程信息创建失败！" && return 1
+    fi
+    read -p "请输入课程信息内容：" mContent
+    echo "m$mid:${mContent}:c${cid}" >> $courseInfoFile # 信息编号、信息内容、课程号写入文件
+    echo "课程信息 [m$mid]$mContent 添加成功，所属课程 c$cid"
+    return 0
+}
 
-# }
+# 修改课程信息，成功返回0、失败返回1。
+modify_course_info() {
+    [ ! -e $courseInfoFile ] && echo "课程信息文件不存在，请联系管理员创建课程！" && return 1    # 检查文件是否存在
+    read -p "请输入欲修改的编号：m" mid
+    minfo=`grep "^m$mid:" $courseInfoFile`  # 试图从文件中找到这个课程信息的一行
+    if [ -z "$minfo" ] ; then   # 若$minfo长度为0，则说明不存在这个编号，报错退出
+        echo "该编号课程信息不存在，修改失败！"
+        return 1
+    fi
+    
+    oldContent=`echo $minfo | cut -d: -f2`
+    oldCID=`echo $minfo | cut -d: -f3`; oldCID=${oldCID:1}
+    read -p "更改所属课程（留空则保持不变）：c$oldCID -> c" cid
+    grep -qs "^c$cid:.*:$1$" $courseInfoFile
+    [ $? -ne 0 ] && echo "您不是该课程的任课老师，修改失败！" && return 1
+    echo "更改课程信息内容（留空则保持不变）：$oldContent ->"
+    read mContent
+    if [ "$mContent" -o "$cid" ] ; then  # 若有任一更改，更新文件
+        sed -i "/^m$mid:/c\m$mid:${mContent:=$oldContent}:c${cid:=$oldCID}" $courseInfoFile
+        echo "编号 m$mid 课程信息信息修改成功！当前课程信息内容：$mContent；当前所属课程：c$cid"
+    else
+        echo "编号 m$mid 课程信息信息未进行任何修改"
+    fi
+    return 0
+}
 
-# modify_course_info() {
+# 删除给定编号的课程信息，成功返回0、失败返回1。
+delete_course_info() {
+    [ ! -e $courseInfoFile ] && echo "课程信息文件不存在，请联系管理员创建课程！" && return 1    # 检查文件是否存在
+    read -p "请输入欲删除的课程信息编号：m" mid
+    grep -qs "^m$mid:" $courseInfoFile # 同上判断课程信息是否存在
+    [ $? -ne 0 ] && echo "该编号课程信息不存在，删除失败！" && return 1
+    cid=`grep "^m$mid:" $courseInfoFile | cut -d: -f3`  # cid="c####"
+    tid=`grep "^$cid:" $courseInfoFile | cut -d: -f3`  # tid="t####"
+    [ "$tid" != "$1" ] && echo "您不是该课程的任课老师，删除失败！" && return 1
+    sed -i "/^m$mid:/d" $courseInfoFile
+    echo "编号 m$mid 课程信息删除成功！"
+    return 0
+}
 
-# }
+# 列出数据库中所有课程信息
+list_course_info() {
+    [ ! -e $courseInfoFile ] && echo "课程信息文件不存在，请联系管理员创建课程！" && return 1    # 检查文件是否存在
+    grep -qs "^m" $courseInfoFile # 判断是否有记录，无记录则报错退出
+    [ $? -ne 0 ] && echo "当前没有任何课程信息" && return 1
+    (echo "编号:课程信息内容:所属课程号" ; grep "^m" $courseInfoFile) | column -ts: # 有记录，加上表头对齐显示输出
+    return 0
+}
 
-# delete_course_info() {
+# 根据给定的编号/课程信息名搜索课程信息
+search_course_info() {
+    [ ! -e $courseInfoFile ] && echo "课程信息文件不存在，请联系管理员创建课程！" && return 1    # 检查文件是否存在
+    read -p "请输入课程信息查找关键字：" mContent
+    grep -qs "^m.*$mContent" $courseInfoFile
+    [ $? -ne 0 ] && echo "没有任何符合条件的课程信息" && return 1
+    
+    (echo "编号:课程信息内容:所属课程号" ; grep "^m.*$mContent" $courseInfoFile) | column -ts:
 
-# }
+    return 0
+}
 
-# list_course_info() {
+# 创建课程作业，成功返回0、失败返回1。
+create_assignment() {
+    [ ! -e $courseInfoFile ] && echo "课程作业文件不存在，请联系管理员创建课程！" && return 1    # 检查文件是否存在
+    read -p "请输入课程作业编号：a" aid     # 为方便区分，课程作业编号前缀'm'
+    grep "^a$aid:" $courseInfoFile 1>/dev/null 2>&1
+    if [ $? -eq 0 ] ; then              # 编号为主键，重复则报错
+        echo "该编号课程作业已存在，创建失败！"
+        return 1
+    fi
+    read -p "请设置该课程作业所属课程：c" cid
+    grep -qs "^c$cid:" $courseInfoFile
+    if [ $? -ne 0 ] ; then
+        echo "该课程不存在，课程作业创建失败！"
+        return 1
+    else
+        grep -qs "^c$cid:.*:$1$" $courseInfoFile
+        [ $? -ne 0 ] && echo "您不是该课程的任课老师，课程作业创建失败！" && return 1
+    fi
+    read -p "请输入课程作业内容：" aContent
+    echo "a$aid:${aContent}:c${cid}" >> $courseInfoFile # 作业编号、作业内容、课程号写入文件
+    echo "课程作业 [a$aid]$aContent 添加成功，所属课程 c$cid"
+    return 0
+}
 
-# }
+# 修改课程作业，成功返回0、失败返回1。
+modify_assignment() {
+    [ ! -e $courseInfoFile ] && echo "课程作业文件不存在，请联系管理员创建课程！" && return 1    # 检查文件是否存在
+    read -p "请输入欲修改的编号：a" aid
+    ainfo=`grep "^a$aid:" $courseInfoFile`  # 试图从文件中找到这个课程作业的一行
+    if [ -z "$ainfo" ] ; then   # 若$ainfo长度为0，则说明不存在这个编号，报错退出
+        echo "该编号课程作业不存在，修改失败！"
+        return 1
+    fi
+    
+    oldContent=`echo $ainfo | cut -d: -f2`
+    oldCID=`echo $ainfo | cut -d: -f3`; oldCID=${oldCID:1}
+    read -p "更改所属课程（留空则保持不变）：c$oldCID -> c" cid
+    grep -qs "^c$cid:.*:$1$" $courseInfoFile
+    [ $? -ne 0 ] && echo "您不是该课程的任课老师，修改失败！" && return 1
+    echo "更改课程作业内容（留空则保持不变）：$oldContent ->"
+    read aContent
+    if [ "$aContent" -o "$cid" ] ; then  # 若有任一更改，更新文件
+        sed -i "/^a$aid:/c\a$aid:${aContent:=$oldContent}:c${cid:=$oldCID}" $courseInfoFile
+        echo "编号 a$aid 课程作业作业修改成功！当前课程作业内容：$aContent；当前所属课程：c$cid"
+    else
+        echo "编号 a$aid 课程作业作业未进行任何修改"
+    fi
+    return 0
+}
 
-# create_assignment() {
+# 删除给定编号的课程作业，成功返回0、失败返回1。
+delete_assignment() {
+    [ ! -e $courseInfoFile ] && echo "课程作业文件不存在，请联系管理员创建课程！" && return 1    # 检查文件是否存在
+    read -p "请输入欲删除的课程作业编号：a" aid
+    grep -qs "^a$aid:" $courseInfoFile # 判断课程作业是否存在
+    [ $? -ne 0 ] && echo "该编号课程作业不存在，删除失败！" && return 1
+    cid=`grep "^a$aid:" $courseInfoFile | cut -d: -f3`  # cid="c####"
+    tid=`grep "^$cid:" $courseInfoFile | cut -d: -f3`  # tid="t####"
+    [ "$tid" != "$1" ] && echo "您不是该课程的任课老师，删除失败！" && return 1
+    sed -i "/^a$aid:/d" $courseInfoFile
+    echo "编号 a$aid 课程作业删除成功！"
+    return 0
+}
 
-# }
+# 列出数据库中所有课程作业
+list_assignments() {
+    [ ! -e $courseInfoFile ] && echo "课程作业文件不存在，请联系管理员创建课程！" && return 1    # 检查文件是否存在
+    grep -qs "^a" $courseInfoFile # 判断是否有记录，无记录则报错退出
+    [ $? -ne 0 ] && echo "当前没有任何课程作业" && return 1
+    (echo "编号:作业内容:所属课程号" ; grep "^a" $courseInfoFile) | column -ts: # 有记录，加上表头对齐显示输出
+    return 0
+}
 
-# modify_assignment() {
+# 根据给定的编号/课程作业名搜索课程作业
+search_assignment() {
+    [ ! -e $courseInfoFile ] && echo "课程作业文件不存在，请联系管理员创建课程！" && return 1    # 检查文件是否存在
+    read -p "请输入作业查找关键字：" aContent
+    grep -qs "^a.*$aContent" $courseInfoFile
+    [ $? -ne 0 ] && echo "没有任何符合条件的课程作业" && return 1
+    
+    (echo "编号:作业内容:所属课程号" ; grep "^a.*$aContent" $courseInfoFile) | column -ts:
 
-# }
-
-# delete_assignment() {
-
-# }
-
-# list_assignments() {
-
-# }
+    return 0
+}
 
 change_teacher_pwd() {
     read -p "原密码：" oldPass
@@ -159,16 +297,21 @@ while true ; do
 
     case $opCode in
         0) exit 0;;   # 返回上级
-        1) create_teacher_account;;
-        2) modify_teacher_account;;
-        3) delete_teacher_account;;
-        4) list_teachers; echo;;
-        5) search_teacher; echo;;
-        6) create_course;;
-        7) modify_course;;
-        8) delete_course;;
-        9) list_courses; echo;;
-        10) search_course; echo;;
+        1) create_student_account;;
+        2) modify_student_account;;
+        3) delete_student_account;;
+        4) list_students; echo;;
+        5) search_student; echo;;
+        6) create_course_info;;
+        7) modify_course_info;;
+        8) delete_course_info;;
+        9) list_course_info; echo;;
+        10) search_course_info; echo;;
+        11) create_assignment;;
+        12) modify_assignment;;
+        13) delete_assignment;;
+        14) list_assignments; echo;;
+        15) search_assignment; echo;;
         16) change_teacher_pwd $1;;
         *)          # 其它输入，报错
             echo "选择无效！请重新输入。"
