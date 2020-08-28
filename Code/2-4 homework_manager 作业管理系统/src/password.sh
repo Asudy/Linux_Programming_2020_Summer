454c1f4423410c59e3f88c74c8a4e9ae7993fc91
Asudy <18906052155@189.cn>
2020-08-28 18:12:24 +0800

    Added comment

--- Code/2-4 homework_manager 作业管理系统/src/password.sh
+++ Code/2-4 homework_manager 作业管理系统/src/password.sh
@@ -6,18 +6,19 @@
 # Author:       Asudy Wang | 王浚哲
 # Student ID:   3180103011
 
-if [ $# -ne 2 ] ; then
+if [ $# -ne 2 ] ; then  # 传入参数个数不对，报错返回
     echo "$0: Check password error: missing or too many arguments"
     return 2
 fi
 
+# 如果还没有用户信息文件，创建并写入管理员初始密码
 [ ! -e $userInfoFile ] && echo "admin:Administrator:123456" > $userInfoFile
-chmod 700 $userInfoFile
+chmod 700 $userInfoFile     # 更改用户信息文件权限
 
-n=`sed -ne "/^\<$1\>/{/\<$2\>$/p}" $userInfoFile | wc -l`
-if [ $n -eq 1 ] ; then
+n=`sed -ne "/^\<$1\>/{/\<$2\>$/p}" $userInfoFile | wc -l`   # 在用户信息文件中查找给出的用户名和密码，统计行数
+if [ $n -eq 1 ] ; then  # 行数为1，则校验成功
     return 0
-else
+else    # 行数不为1，则校验失败
     [ "$DEBUG" ] && echo "check_pwd: User = $1, Pass = $2, Lines = $n"
     return 1
 fi
